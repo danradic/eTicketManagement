16357f0bfb4fe1bdc4d040da2aa23e3a66ad013c
danradic <danradic@gmail.com>
2022-08-22 18:19:06 +0200

    add Request/Handler for event details in Application project

---
+++ src/Core/GloboTicket.TicketManagement.Application/Features/Events/CategoryDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events.GetEventDetail
+{
+    public class CategoryDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/Core/GloboTicket.TicketManagement.Application/Features/Events/GetEventDetailQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events.GetEventDetail
+{
+    public class GetEventDetailQuery: IRequest<EventDetailVm>
+    {
+        public Guid Id { get; set; }
+    }
+}
