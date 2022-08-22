126d25aaf504c8adfbc6af1505f5373b2476c51e
danradic <danradic@gmail.com>
2022-08-22 18:06:03 +0200

    add AutoMapper Profile in Application project

---
+++ src/Core/GloboTicket.TicketManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Features.Events.GetEventsList;
+using GloboTicket.TicketManagement.Domain.Entities;
+
+namespace GloboTicket.TicketManagement.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Event, EventListVm>();
+        }
+    }
+}
