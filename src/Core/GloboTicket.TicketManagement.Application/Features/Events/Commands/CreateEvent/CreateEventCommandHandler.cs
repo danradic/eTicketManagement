d69ea9c89f8edca6029dfe0872e1929cbc32bf1e
danradic <danradic@gmail.com>
2022-08-25 11:22:54 +0200

    check validation result for errors

--- src/Core/GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
+++ src/Core/GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -22,6 +22,9 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
             var validator = new CreateEventCommandValidator();
             var validationResult = await validator.ValidateAsync(request);
 
+            if (validationResult.Errors.Count > 0)
+                throw new Exceptions.ValidationException(validationResult);
+
             var @event = _mapper.Map<Event>(request);
 
             @event = await _eventRepository.AddAsync(@event);
