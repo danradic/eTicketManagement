a70ebe4d0dc6cc8da4c2224ab0f52697e04fc8cc
danradic <danradic@gmail.com>
2022-08-26 19:43:48 +0200

    add repositories

---
+++ src/Infrastructure/GloboTicket.TicketManagement.Persistence/Repositories/EventRepository.cs
@@ -0,0 +1,21 @@
+﻿using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Persistence.Repositories
+{
+    public class EventRepository : BaseRepository<Event>, IEventRepository
+    {
+        public EventRepository(GloboTicketDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public Task<bool> IsEventNameAndDateUnique(string name, DateTime eventDate)
+        {
+            var matches =  _dbContext.Events.Any(e => e.Name.Equals(name) && e.Date.Date.Equals(eventDate.Date));
+            return Task.FromResult(matches);
+        }
+    }
+}
---
+++ src/Infrastructure/GloboTicket.TicketManagement.Persistence/Repositories/OrderRepository.cs
@@ -0,0 +1,28 @@
+﻿using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Persistence.Repositories
+{
+    public class OrderRepository : BaseRepository<Order>, IOrderRepository
+    {
+        public OrderRepository(GloboTicketDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<List<Order>> GetPagedOrdersForMonth(DateTime date, int page, int size)
+        {
+            return await _dbContext.Orders.Where(x => x.OrderPlaced.Month == date.Month && x.OrderPlaced.Year == date.Year)
+                .Skip((page - 1) * size).Take(size).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<int> GetTotalCountOfOrdersForMonth(DateTime date)
+        {
+            return await _dbContext.Orders.CountAsync(x => x.OrderPlaced.Month == date.Month && x.OrderPlaced.Year == date.Year);
+        }
+    }
+}
