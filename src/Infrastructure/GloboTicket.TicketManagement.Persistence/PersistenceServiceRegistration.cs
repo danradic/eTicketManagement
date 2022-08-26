d1745780e6d17e432802c44c8700a7d96de2c171
danradic <danradic@gmail.com>
2022-08-26 20:21:59 +0200

    add PersistenceServiceRegistration class

---
+++ src/Infrastructure/GloboTicket.TicketManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,30 @@
+﻿using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<GloboTicketDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("GloboTicketTicketManagementConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IEventRepository, EventRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            return services;
+        }
+    }
+}
