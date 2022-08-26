b80977321452af3c747904dc5639113316ee55f8
danradic <danradic@gmail.com>
2022-08-26 22:08:22 +0200

    add IEmailService contract, Email and EmailSettings models

---
+++ src/Core/GloboTicket.TicketManagement.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using GloboTicket.TicketManagement.Application.Models.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Contracts.Infrastructure
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
