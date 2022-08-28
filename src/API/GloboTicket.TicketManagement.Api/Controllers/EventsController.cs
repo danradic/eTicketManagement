5b6e25c8a8eb4b6c091399b4286ff9f8e315dea4
danradic <danradic@gmail.com>
2022-08-28 15:20:33 +0200

    add MediatR package, API endpoints

---
+++ src/API/GloboTicket.TicketManagement.Api/Controllers/EventsController.cs
@@ -0,0 +1,45 @@
+﻿using GloboTicket.TicketManagement.Application.Features.Events.Commands.CreateEvent;
+using GloboTicket.TicketManagement.Application.Features.Events.Commands.DeleteEvent;
+using GloboTicket.TicketManagement.Application.Features.Events.Commands.UpdateEvent;
+using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventDetail;
+using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsList;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GloboTicket.TicketManagement.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventsController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public EventsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet(Name = "GetAllEvents")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<List<EventListVm>>> GetAllEvents()
+        {
+            var dtos = await _mediator.Send(new GetEventsListQuery());
+            return Ok(dtos);
+        }
+
+        [HttpGet("{id}", Name = "GetEventById")]
+        public async Task<ActionResult<EventDetailVm>> GetEventById(Guid id)
+        {
+            var getEventDetailQuery = new GetEventDetailQuery() { Id = id };
+            return Ok(await _mediator.Send(getEventDetailQuery));
+        }
+
+        [HttpPost(Name = "AddEvent")]
+        public async Task<ActionResult<Guid>> Create([FromBody] CreateEventCommand createEventCommand)
+        {
+            var id = await _mediator.Send(createEventCommand);
+            return Ok(id);
+        }
+    }
+}
---
+++ src/API/GloboTicket.TicketManagement.Api/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using GloboTicket.TicketManagement.Application.Features.Orders.GetOrdersForMonth;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GloboTicket.TicketManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("/getpagedordersformonth", Name = "GetPagedOrdersForMonth")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<PagedOrdersForMonthVm>> GetPagedOrdersForMonth(DateTime date, int page, int size)
+        {
+            var getOrdersForMonthQuery = new GetOrdersForMonthQuery() { Date = date, Page = page, Size = size };
+            var dtos = await _mediator.Send(getOrdersForMonthQuery);
+
+            return Ok(dtos);
+        }
+    }
+}
