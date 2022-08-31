8ead19929c92ad825f411b019c1f5df5db8f5956
danradic <danradic@gmail.com>
2022-08-31 20:27:34 +0200

    add unit tests for create/get category handlers

---
+++ tests/GloboTicket.TicketManagement.Application.UnitTests/Categories/Queries/GetCategoriesListQueryHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
+using GloboTicket.TicketManagement.Application.Profiles;
+using GloboTicket.TicketManagement.Application.UnitTests.Mocks;
+using GloboTicket.TicketManagement.Domain.Entities;
+using Moq;
+using Shouldly;
+
+namespace GloboTicket.TicketManagement.Application.UnitTests.Categories.Queries
+{
+    public class GetCategoriesListQueryHandlerTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<IAsyncRepository<Category>> _mockCategoryRepository;
+
+        public GetCategoriesListQueryHandlerTests()
+        {
+            var configurationProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+            _mapper = configurationProvider.CreateMapper();
+
+            _mockCategoryRepository = RepositoryMocks.GetCategoryRepository(); ;
+        }
+
+
+        [Fact]
+        public async Task GetCategoriesListAsync() 
+        {
+            var handler = new GetCategoriesListQueryHandler(_mapper, _mockCategoryRepository.Object);
+
+            var result = await handler.Handle(new GetCategoriesListQuery(), CancellationToken.None);
+
+            result.ShouldBeOfType<List<CategoryListVm>>();
+
+            result.Count.Equals(4);
+        }
+    }
+}
