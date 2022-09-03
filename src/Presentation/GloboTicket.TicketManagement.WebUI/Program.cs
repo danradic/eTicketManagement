7645f9e87b1e96ead29a00b5557105d57172c2e7
danradic <danradic@gmail.com>
2022-09-03 14:13:38 +0200

    register HttpClient generated via NSwagStudio in Program.cs of Blazor UI

--- src/Presentation/GloboTicket.TicketManagement.WebUI/Program.cs
+++ src/Presentation/GloboTicket.TicketManagement.WebUI/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using GloboTicket.TicketManagement.WebUI;
+using GloboTicket.TicketManagement.WebUI.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
 await builder.Build().RunAsync();
