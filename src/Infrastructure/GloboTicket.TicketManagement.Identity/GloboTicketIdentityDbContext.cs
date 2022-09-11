b81f37557dc3714468d11e3dba825a936b73476b
danradic <danradic@gmail.com>
2022-09-11 17:16:26 +0200

    add Identity Migrations

--- src/Infrastructure/GloboTicket.TicketManagement.Identity/GloboTicketIdentityDbContext.cs
+++ src/Infrastructure/GloboTicket.TicketManagement.Identity/GloboTicketIdentityDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using GloboTicket.TicketManagement.Identity.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,29 @@ namespace GloboTicket.TicketManagement.Identity
     {
         public GloboTicketIdentityDbContext(DbContextOptions<GloboTicketIdentityDbContext> options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            SeedUsers(builder);
+        }
+
+        private void SeedUsers(ModelBuilder builder)
+        {
+            ApplicationUser user = new ApplicationUser()
+            {
+                FirstName = "John",
+                LastName = "Smith",
+                UserName = "johnsmith",
+                Email = "john@test.com",
+                EmailConfirmed = true
+            };
+
+            PasswordHasher<ApplicationUser> passwordHasher = new PasswordHasher<ApplicationUser>();
+            passwordHasher.HashPassword(user, "Admin*123");
+
+            builder.Entity<ApplicationUser>().HasData(user);
         }
     }
 }
